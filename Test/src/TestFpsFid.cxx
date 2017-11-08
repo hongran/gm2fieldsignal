48938223e8c14f0d5fac384a65a386cdf87ffe87
Ran Hong <hongran@uw.edu>
2017-11-07 20:41:52 -0600

    update

--- Test/src/TestFpsFid.cxx
+++ Test/src/TestFpsFid.cxx
@@ -7,17 +7,18 @@
 #include <chrono>
 
 #include "fid.h"
+#include <stdlib.h>
 
-int main(){
+int main(int argc,char ** argv){
   TGraph * gWf = new TGraph();
 
-  int run  = 1280;
+  int run  = atoi(argv[1]);
 
   TFile* filein = new TFile(Form("data/FpsFidGraphOut%05d_tier0.root",run),"read");
   TDirectory * d1 = (TDirectory*)filein->Get("PlotFixedProbeFid");
   gROOT->cd();
-  int EventID = 2;
-  int ProbeID = 150;
+  int EventID = atoi(argv[2]);
+  int ProbeID = atoi(argv[3]);
   TGraph *g = (TGraph*)d1->Get(Form("Event_%03d_probe_%03d",EventID,ProbeID));
   gWf = (TGraph*)g->Clone();
 
@@ -65,7 +66,12 @@ int main(){
   myFid.Init("Standard");
   std::cout<<"PD: " << myFid.GetFreq("PD")<<std::endl;
   std::cout<<"ZC: " << myFid.GetFreq("ZC")<<std::endl;
-//  std::cout << myFid.GetFreq("LZ")<<std::endl;
+  auto t0lz = std::chrono::high_resolution_clock::now();
+  std::cout<<"LZ: " << myFid.GetFreq("LZ")<<std::endl;
+  auto t1lz = std::chrono::high_resolution_clock::now();
+  auto dtnlz = t1lz.time_since_epoch() - t0lz.time_since_epoch();
+  double tlz = std::chrono::duration_cast<std::chrono::nanoseconds>(dtnlz).count();
+  std::cout << "Time LZ = "<<tlz<<std::endl;
   std::cout << myFid.amp()<<std::endl;
   std::cout << myFid.snr()<<std::endl;
 //  myFid.CalcLorentzianFreq();
